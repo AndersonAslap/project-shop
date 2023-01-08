374000d6278c7fc7daebb61d4494c9a57305aa65
AndersonAslap <anderson.adolfo1998@gmail.com>
2023-01-08 01:45:53 -0300

    feat: product detail

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,14 @@ import 'keen-slider/keen-slider.min.css'
 import { stripe } from "../lib/stripe"
 import { GetStaticProps } from "next"
 import Stripe from "stripe"
+import Link from "next/link"
 
 interface HomeProps {
   products : {
     id: string;
     name: string;
     imageUrl: string;
-    price: number;
+    price: string;
   }[]
 }
 
@@ -34,21 +35,29 @@ export default function Home({ products }: HomeProps) {
      <HomeContainer ref={sliderRef} className='keen-slider'>
       
       {products.map(product => (
-        <Product className='keen-slider__slide' key={product.id}>
-          <Image 
-            src={product.imageUrl} 
-            width={520} 
-            height={480} 
-            alt=''  
-          />
+        <Link 
+          key={product.id}
+          href={`/product/${product.id}`}
+          prefetch={false}
+        >
+          <Product 
+            className='keen-slider__slide' 
+          >
+            <Image 
+              src={product.imageUrl} 
+              width={520} 
+              height={480} 
+              alt=''  
+            />
 
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        </Link>
       ))}
-
+      
      </HomeContainer>
     </>
   )
